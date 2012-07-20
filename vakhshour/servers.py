ac2f0079dc51198842175d60fd273d4435f8844c
Sameer Rahmani <lxsameer@gnu.org>
2012-07-20 13:18:23 +0430

    Event Pull/Sub threads done. (not tested)

--- vakhshour/servers.py
+++ vakhshour/servers.py
@@ -17,6 +17,8 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
 import logging
+from Queue import Queue
+from threading import Thread
 
 import zmq
 
@@ -44,16 +46,18 @@ class Server(VObject):
         ip = self.config.get("ip", address)
         return "tcp://%s:%s" % (ip, port)
 
-    def _establish_pull_push(self):
+    def _establish_pull_push(self, push_host=None, pull_host=None):
         # Establish PULL/PULL sockets
         self.pullsocket = self.context.socket(zmq.PULL)
         self.pushsocket = self.context.socket(zmq.PUSH)
 
+        host = pull_host or self._default_ip
         port = self.config.get("pull_port", self._pull_port)
-        self.logger.debug("Binding PULL to %s" % self._address(port))
+        self.logger.debug("Binding PULL to %s" % self._address(port, host))
         self.pullsocket.bind(self._address(port))
 
-        self.logger.debug("Binding PSHL to %s" % self._address(port))
+        host = push_host or self._default_ip
+        self.logger.debug("Binding PUSH to %s" % self._address(port, host))
         port = self.config.get("push_port", self._push_port)
         self.pushsocket.bind(self._address(port))
 
@@ -102,23 +106,61 @@ class EventSubscriber(Server):
 
     _default_ip = "127.0.0.1"
 
+    # {"event": [("project.event.handler", "project path")]
+    _register = {}
+
     def __init__(self, *args, **kwargs):
         super(EventSubscriber, self).__init__(*args, **kwargs)
 
-        self._establish_pull_push()
+        self._establish_pull_push(push_host="127.0.0.1",
+                                  pull_host="127.0.0.1")
 
-        # Establish publisher socket
-        self.subsocket = self.context.socket(zmq.SUB)
-        port = self.config.get("sub_port", self._sub_port)
-        self.subsocket.connect(self._address(port))
-        self.subsocket.setsockopt(zmq.SUBSCRIBE, "")
+        self._establish_subscriber()
+        self.queue = Queue()
+
+        self.subscriber_thread = Thread(target=self._subscriber_loop)
+        self.register_events_thread = Thread(target=self._register_events_loop)
+
+        self.subscriber_thread.daemon = True
+        self.register_events_thread.daemon = True
+
+    def _subscriber_loop(self):
+        """
+        Subscriber thread.
+        """
+        while True:
+            self.logger.debug("Entering event receiveing loop")
+            data = self.subsocket.recv()
+            self.logger.info("EVENT RECV: %s" % data)
+            self.queue.put(data)
+
+    def _register_events_loop(self):
+        """
+        Event registering thread.
+        """
+        while True:
+            self.logger.debug("Entering event registering loop")
+            data = self.pullsocket.recv()
+            self.logger.info("PULL RECV: %s" % data)
+            self.queue.put(data)
 
     def run(self):
         """
         Main method that is responsible for server run.
         """
+        self.subscriber_thread.start()
         self.logger.info("EventSubscriber is running.")
-        while True:
-            self.logger.debug("Entering event loop")
-            data = self.subsocket.recv()
-            self.logger.info("RECV: %s" % data)
+        self.register_events_thread.start()
+        self.logger.info("Event registeration is running.")
+        while 1:
+            task = self.queue.get()
+            print task
+
+    def _establish_subscriber(self):
+        """
+        Establish publisher socket
+        """
+        self.subsocket = self.context.socket(zmq.SUB)
+        port = self.config.get("sub_port", self._sub_port)
+        self.subsocket.connect(self._address(port))
+        self.subsocket.setsockopt(zmq.SUBSCRIBE, "")
