52fa19178a6773c25667d74e36078fcf73754281
Sameer Rahmani <lxsameer@gnu.org>
2012-07-22 17:25:52 +0430

    setup.py added

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#    Vakhshour - Event and Message layer application
+#    Copyright (C) 2012 Yellowen
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+
+from distutils.core import setup
+
+setup(name='Vakhshour',
+      version='0.1.0',
+      description='Event and Message layer application',
+      author='Sameer Rahmani',
+      author_email='lxsameer@gnu.org',
+      url='http://issue.yellowen.com/projects/vakhshur',
+      license='GPL v2',
+      scripts=["vakhshourd"],
+      #data_files=[("/etc/init.d/", ["conf/vakhshour.json"]), ],
+      packages=['vakhshour'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+          ]
+)
---
+++ vakhshour/discovery.py
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------------------------
+#    Vakhshour - Event and Message layer application
+#    Copyright (C) 2012 Yellowen
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+
+from base import Packet
+
+
+def autodiscovery(installed_apps):
+    for app in installed_apps:
+        module = __import__("%s.handlers" % app,
+                            globals(),
+                            locals(),
+                            [],
+                            -1)
+        print ">>>> ", module
+
