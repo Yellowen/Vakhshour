178e524f2c437fd8ee322d7370c0ea8f9f426878
Sameer Rahmani <lxsameer@gnu.org>
2012-08-17 16:13:52 +0430

    last changes before moving to bitbucket

--- vakhshour/servers.py
+++ vakhshour/servers.py
@@ -17,8 +17,9 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
 import logging
+from OpenSSL import SSL
 
-from twisted.internet import reactor
+from twisted.internet import reactor, ssl
 
 from base import VObject, Event
 from protocols import EventTransportFactory, EventPublisherFactory
@@ -28,7 +29,7 @@ class PublisherServer(VObject):
 
     def __init__(self, config={}, host="127.0.0.1", pub_port="7777",
                  recv_port="8888", secure=False,
-                 sslkey=None, sslcert=None):
+                 sslkey=None, sslcert=None, cacert=None):
 
         self.config = config
         self.host = self.config.get("host", host)
@@ -37,15 +38,40 @@ class PublisherServer(VObject):
         self.secure = self.config.get("secure", secure)
         self.key = self.config.get("ssl_key", sslkey)
         self.cert = self.config.get("ssl_cert", sslcert)
+        self.cacert = self.config.get("ca_cert", cacert)
+
+        self.publisher = EventPublisherFactory()
+        self.event_receiver = EventTransportFactory(self.publisher)
 
         if self.secure:
-            pass
-        else:
-            self.publisher = EventPublisherFactory()
-            reactor.listenTCP(int(pub_port), self.publisher)
+            ContextFactory = ssl.DefaultOpenSSLContextFactory(self.key,
+                                                              self.cert)
 
-            self.event_receiver = EventTransportFactory(self.publisher)
-            reactor.listenTCP(int(recv_port), self.event_receiver)
+            ctx = ContextFactory.getContext()
+
+            ctx.set_verify(
+                SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT,
+                self.verify_cert,
+            )
+
+            # Since we have self-signed certs we have to explicitly
+            # tell the server to trust them.
+            ctx.load_verify_locations(self.cacert)
+
+            reactor.listenSSL(
+                int(self.recvport),
+                self.event_receiver,
+                ContextFactory)
+
+            reactor.listenSSL(
+                int(self.pubport),
+                self.publisher,
+                ContextFactory)
+        else:
+            reactor.listenTCP(int(pub_port),
+                              self.publisher)
+            reactor.listenTCP(int(recv_port),
+                              self.event_receiver)
 
     def run(self):
         if self.secure:
@@ -60,3 +86,12 @@ class PublisherServer(VObject):
                 self.recvport))
 
         reactor.run()
+
+    def verify_cert(self, connection, x509, errnum, errdepth, ok):
+        if not ok:
+            self.logger.error(
+                'invalid cert from subject: %s' % x509.get_subject())
+            return False
+        else:
+            self.logger.info("Cert is fine.")
+            return True
